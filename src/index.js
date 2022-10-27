db9ef51ebafb0ed94b3be1e8486c8056fb221001
omarashry98 <81537403+omarashry98@users.noreply.github.com>
2022-10-27 11:01:14 -0400

    adding login and register api

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const express = require('express');
+const userRouter = require('./routers/user');
+require('dotenv').config();
+const connectionString = process.env.API_KEY;
+
+const app = express();
+
+mongoose.connect(connectionString);
+
+
+app.use(async (req, res, next) => {
+    try {
+        return await next();
+    } catch (error) {
+        console.log("Error caight in middleware", error);
+    }
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.use("/user", userRouter);
+
+app.listen(3000, ()=>{console.log(`Server running at port 3000`)});
+
+
