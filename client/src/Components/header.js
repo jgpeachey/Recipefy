4c59d975d441193b5dde5a1bf861295c14b0619a
Alexander Williams <alexander.williams@knights.ucf.edu>
2022-10-31 16:27:41 -0400

    Started Login

---
+++ client/src/Components/header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <div className = "Login-Page">
+            <h1 className = "Login-Header">Recipefy</h1>
+        
+        </div>
+    )
+}
---
+++ client/src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+import Header from './Components/header.js';
+
+// Login component
+
+function Login(){
+    return(
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Login;
