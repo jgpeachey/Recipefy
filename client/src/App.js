8ee3709b127ec397fba609dc036b0510365e3351
Rithvik Dinesh <hellorithvik14@gmail.com>
2022-11-02 19:05:18 -0400

    updated names

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,26 @@
-import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 
-function App() {}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+);
 
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
