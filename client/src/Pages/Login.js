c2f3c779f0e2aaa0c9ef8e8016f7dfe4d381faf4
Rithvik Dinesh <hellorithvik14@gmail.com>
2022-11-03 12:25:22 -0400

    trying with navigate

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -180,7 +180,7 @@ export default function Login() {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="/register" variant="body2">
+                  <Link onClick={navigate("/register")} variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
