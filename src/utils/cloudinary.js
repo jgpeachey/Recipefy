4bf555c67efdf6811c6e1e92925e69efe376b5ef
Jason Lin <jasonlinfl@gmail.com>
2022-11-03 15:36:05 -0400

    changed cloudinary env keys

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  cloud_key: process.env.CLOUD_KEY,
-  api_secret: process.env.CLOUD_KEY_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  cloud_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
