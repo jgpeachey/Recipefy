b36ca0593ef0c297cb234b3e222548391732fcf5
Alexander Williams <alexander.williams@knights.ucf.edu>
2022-11-04 04:34:47 -0400

    made better carousel

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -7,8 +7,13 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { fontFamily } from "@mui/system";
 import Carousel from "react-material-ui-carousel";
 import { Grid } from "@mui/material";
+
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 //import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 
 const theme = createTheme({});
@@ -19,22 +24,76 @@ export default function Home() {
     <ThemeProvider theme={theme}>
       <HomeAppBar/>
       
-      <Carousel
-      sx={{ mt: 18, mb: 0 , ml: 45, mr: 45}}
+      <Carousel className="Carousel"
+      sx={{ mt:0, mb: 0 , ml: 40, mr: 50}}
+      navButtonsAlwaysVisible={true}
+      animation="slide"
+      autoPlay={false}
       >  
-        <div>  
-            <img src="https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/milk-dairy/2-1-3-1dairyfoods_cheese_detailfeature_thumb.jpg?sfvrsn=aba8c621_4"/>  
-            <p className="legend">Cheese</p>  
-        </div>  
-        <div>  
-            <img src="https://assets.bonappetit.com/photos/5c62e4a3e81bbf522a9579ce/5:4/w_2815,h_2252,c_limit/milk-bread.jpg" />  
-            <p className="legend">Food</p>  
-        </div>  
-        <div>  
-            <img src="https://i1.rgstatic.net/ii/profile.image/730557839319044-1551189970680_Q512/Rick-Leinecker.jpg" />  
-            <p className="legend">Richard</p>  
-        </div>  
-        </Carousel>  
+        <div>
+          <Card sx={{ maxWidth: 500 }} className="Card">
+            <CardMedia
+              component="img"
+              height="150"
+              image="https://www.starfrit.com/media/contentmanager/content/cache/1070x1070/recipes/e1_r1_spaghetti.jpg"
+              alt="spag"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Spaghet
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Spaghetti is fire. Lemme show you how.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">View Recipe</Button>
+            </CardActions>
+          </Card>
+        </div>
+        <div>
+          <Card sx={{ maxWidth: 500 }} className="Card">
+            <CardMedia
+              component="img"
+              height="150"
+              image="https://www.melskitchencafe.com/wp-content/uploads/french-bread2.jpg"
+              alt="brad"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Bred
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Bread tastes yummy and carbs
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">View Recipe</Button>
+            </CardActions>
+          </Card>
+        </div>
+        <div>
+          <Card sx={{ maxWidth: 500 }} className="Card">
+            <CardMedia
+              component="img"
+              height="150"
+              image="https://www.acouplecooks.com/wp-content/uploads/2021/09/Tomato-Soup-002s.jpg"
+              alt="soup"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Zoop
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Slurp
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">View Recipe</Button>
+            </CardActions>
+          </Card>
+        </div>
+        </Carousel>
     </ThemeProvider>
     
   );
