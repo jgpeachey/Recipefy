ac4a766c28d95e48b3b6b277eaafa5278329ed26
Alexander Williams <alexander.williams@knights.ucf.edu>
2022-11-07 04:18:23 -0500

    Better carousel

---
+++ client/src/Components/SliderData.js
@@ -0,0 +1,11 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+    }
+]
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,23 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import Typography from "@mui/material/Typography";
-import Axios from "axios";
 import HomeAppBar from "../Components/HomeAppBar";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { fontFamily } from "@mui/system";
-import Carousel from "react-material-ui-carousel";
 import { Grid } from "@mui/material";
 import { Container } from "@mui/material";
-import Box from "@mui/material/Box";
 
-import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import RecipeCard from "../Components/RecipeCard";
-//import "react-responsive-carousel/lib/styles/carousel.min.css";
+import ImageCarousel from '../Components/ImageCarousel'
+
+import { SliderData } from '../Components/SliderData';
 
 const theme = createTheme({});
 
@@ -26,53 +16,8 @@ export default function Home() {
     <ThemeProvider theme={theme}>
       <HomeAppBar />
 
-      <Carousel
-        className="Carousel"
-        sx={{ mt: 0, mb: 0, ml: 40, mr: 50 }}
-        navButtonsAlwaysVisible={true}
-        animation="slide"
-        autoPlay={false}
-      >
-        <div>
-          <Card sx={{ maxWidth: 500 }} className="Card">
-            <CardMedia
-              component="img"
-              height="150"
-              image="https://www.starfrit.com/media/contentmanager/content/cache/1070x1070/recipes/e1_r1_spaghetti.jpg"
-              alt="spag"
-            />
-            <CardActions>
-              <Button size="small">View Recipe</Button>
-            </CardActions>
-          </Card>
-        </div>
-        <div>
-          <Card sx={{ maxWidth: 500 }} className="Card">
-            <CardMedia
-              component="img"
-              height="150"
-              image="https://www.melskitchencafe.com/wp-content/uploads/french-bread2.jpg"
-              alt="brad"
-            />
-            <CardActions>
-              <Button size="small">View Recipe</Button>
-            </CardActions>
-          </Card>
-        </div>
-        <div>
-          <Card sx={{ maxWidth: 500 }} className="Card">
-            <CardMedia
-              component="img"
-              height="150"
-              image="https://www.acouplecooks.com/wp-content/uploads/2021/09/Tomato-Soup-002s.jpg"
-              alt="soup"
-            />
-            <CardActions>
-              <Button size="small">View Recipe</Button>
-            </CardActions>
-          </Card>
-        </div>
-      </Carousel>
+      <ImageCarousel slides={SliderData}/>
+
       <Container>
         <Grid container spacing={11} marginTop={-8.5}>
           <RecipeCard />
