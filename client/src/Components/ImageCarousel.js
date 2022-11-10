f730b4fc499ac6c1f327c299b24bb3b269e1dadd
Alexander Williams <alexander.williams@knights.ucf.edu>
2022-11-10 00:58:11 -0500

    skeleton of modal finished. Click image on carousel to view

--- client/src/Components/ImageCarousel.js
+++ client/src/Components/ImageCarousel.js
@@ -1,15 +1,25 @@
 import React, {useState} from 'react'
 import { SliderData } from './SliderData';
 import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
-import { Button } from '@mui/material';
+import { Button, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import Modal from './Modal';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { maxWidth } from '@mui/system';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+const theme = createTheme({});
 
 const ImageCarousel = ({slides}) => {
   const[current, setCurrent] = useState(0)
   const length = slides.length
   const navigate = useNavigate();
-  const[openModal, setOpenModal] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const nextSlide = () => {
     setCurrent(current === length - 1 ? 0 : current + 1)
@@ -19,28 +29,74 @@ const ImageCarousel = ({slides}) => {
     setCurrent(current === 0 ? length - 1 : current - 1)
   }
 
-  const navigateToRecipe = () =>{
-    
-  }
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   if(!Array.isArray(slides) || slides.length <= 0){
     return
   }
 
   return (
-    <section className="slider">
-      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
-      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
-      {SliderData.map((slide, index) => {
-        return(
-          <div className={index === current ? 'slide active' : 'slide'} key={index}>
-            {index === current && (<img src={slide.image} alt='food' className='image' onClick={() => {setOpenModal(true)}}/>)}
-            {openModal && <Modal closeModal={setOpenModal}/>}
+    <ThemeProvider theme={theme}>
+      <section className="slider">
+        <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
+        <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
+        <Dialog
+          open={open}
+          keepMounted
+          onClose={handleClose}
+          maxWidth={maxWidth}
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <div className='modalContainerTop'>
+            <DialogTitle sx={{ color: 'white' }}>{"Steak Stir Fry"}</DialogTitle>
+            <DialogContentText sx={{ color: 'white' }}>
+              by: Recipe_Creator
+            </DialogContentText>
+            <AccountCircleIcon sx={{ color: 'white' }} className="modalPFP"/>
+            <Button sx={{ color:'white' , pl:2 }}>Follow+</Button>
           </div>
-        )
+          <DialogContent>
+            
+            <div className='modalContainer'>
+              <img className="modalImg" src='https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/>
+              <DialogContent>
+                <DialogTitle className="ingredientHeader">
+                  Ingredients:
+                </DialogTitle>
+                <DialogContentText className="recipeText">
+                  cumin, water, pork, mushrooms, cashews, onions
+                </DialogContentText>
+                <DialogTitle>
+                  Instructions:
+                </DialogTitle>
+                <DialogContentText className="recipeText">
+                  This is my recipe! It is really yummy and I like to cook it and it is a really good snack to eat. It is also nutritious. Loren ipsum.<br/>
+                  1.<br/>
+                  2.<br/>
+                  3.<br/>
+                </DialogContentText>
+              </DialogContent>
+            </div>
+    
+          </DialogContent>
+        </Dialog>
+        {SliderData.map((slide, index) => {
+          return(
+            <div className={index === current ? 'slide active' : 'slide'} key={index}>
+              {index === current && (<img src={slide.image} alt='food' className='image' onClick={() => setOpen(true)}/>)}
+            </div>
+          )
 
-      })}
-    </section>   
+        })}
+      </section>
+
+    </ThemeProvider>
   )
 }
 
