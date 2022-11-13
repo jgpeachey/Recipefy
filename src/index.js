ffe20da4154d339af39d5124a35ccb51ae6d543c
Rithvik Dinesh <hellorithvik14@gmail.com>
2022-11-13 01:36:14 -0500

    idk

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 // );
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(__dirname + "/client/build"));
+  app.use(express.static("/client/build"));
 
   // app.get("/", (req, res) => {
   //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
