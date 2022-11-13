e97a3eccb598bad526a529d07e4dd773e9825d40
Rithvik Dinesh <hellorithvik14@gmail.com>
2022-11-13 01:50:09 -0500

    one last try

--- src/index.js
+++ src/index.js
@@ -45,7 +45,14 @@ if (process.env.NODE_ENV === "production") {
   // });
 
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.join(__dirname, "client", "build", "index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
   });
   /*app.get("/forgotpassword", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
