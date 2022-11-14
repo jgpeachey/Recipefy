78bd2aff33ad4834af33d2dc26d8db765af2a82d
Rithvik Dinesh <hellorithvik14@gmail.com>
2022-11-14 13:11:36 -0500

    added loading

---
+++ client/src/Pages/Loading.js
@@ -0,0 +1,25 @@
+import { Axios } from "axios";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+export default function Loading() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const app_name = "recipefy-g1";
+  function buildPath(route) {
+    if (process.env.NODE_ENV === "production") {
+      return "https://" + app_name + ".herokuapp.com/" + route;
+    } else {
+      return "http://localhost:3001/" + route;
+    }
+  }
+  useEffect(() => {
+    Axios.get(buildPath("user/verify/" + searchParams.get("token")))
+      .then((response) => {
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/");
+      });
+  }, []);
+}
