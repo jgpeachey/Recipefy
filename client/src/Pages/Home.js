f3c6c149af1fc486454c4b855b208d21417132c9
psottomayorpereir <itapeu16@hotmail.com>
2022-11-16 01:08:55 -0500

    Recipe card updated, add recipe updated

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -32,6 +32,7 @@ export default function Home() {
   const [openProfile, setOpenProfile] = useState(false);
   const [searcher, setSearcher] = useState("");
   const [change, setChange] = useState(false);
+  var counter = 0;
 
   const appbarToHome = (appbardata) => {
     console.log(appbardata);
@@ -80,7 +81,10 @@ export default function Home() {
         .then((response) => {
           console.log(response);
           var res = [];
+          counter = recipeCardsArray.length;
           for (let q = 0; q < response.data.results.length; q++) {
+            response.data.results[q].index = counter;
+            counter++;
             res.push(response.data.results[q]);
           }
           if (page === 1) {
@@ -113,7 +117,10 @@ export default function Home() {
         .then((response) => {
           console.log(response);
           var res = [];
+          counter = recipeCardsArray.length;
           for (let q = 0; q < response.data.results.length; q++) {
+            response.data.results[q].index = counter;
+            counter++;
             res.push(response.data.results[q]);
           }
           if (res.length != 0) {
@@ -146,6 +153,7 @@ export default function Home() {
 
   useEffect(() => {
     console.log("called");
+    console.log(page);
     getRecipes();
   }, [searcher, change]);
 
