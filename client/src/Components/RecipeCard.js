46639469d05c221d4a6a0ba1e581571990fe18b9
Alexander Williams <alexander.williams@knights.ucf.edu>
2022-11-19 23:27:41 -0500

    finished follow/unfollow api in frontend

--- client/src/Components/RecipeCard.js
+++ client/src/Components/RecipeCard.js
@@ -18,6 +18,7 @@ import Container from "@mui/system/Container";
 
 import Axios from "axios";
 import { cookies, useCookies } from "react-cookie";
+import axios from "axios";
 
 const theme = createTheme({
   typography: {
@@ -63,6 +64,14 @@ export default function RecipeCard({
   const [clickedUser, setClickedUser] = useState(0);
   const [openProfile, setOpenProfile] = useState(false);
 
+  const [id, setId] = useState([]); // all recipe ids
+  const [likedId, setLikedId] = useState([]); // all liked recipe ids
+  const [liked, setLiked] = useState(false); // if recipe is liked or not
+
+  const [idToFollow, setIdToFollow] = useState("");
+  const [followChange, setFollowChange] = useState(false);
+
+
   // console.log(recipe);
   const app_name = "recipefy-g1";
   const [cookies, setCookie] = useCookies(["user"]);
@@ -80,6 +89,10 @@ export default function RecipeCard({
 
   const handleClose2 = () => {
     setOpenProfile(false);
+  };
+
+  const followChanger = () => {
+    setFollowChange(!followChange);
   };
 
   function buildPath(route) {
@@ -157,6 +170,7 @@ export default function RecipeCard({
         console.log(response.data.results[0]);
         userToGet = response.data.results[0]._id;
         setPfp(response.data.results[0].Pic);
+        setIdToFollow(response.data.results[0]._id);
 
         console.log(userToGet);
         console.log(pfp);
@@ -215,6 +229,151 @@ export default function RecipeCard({
       });
   }
 
+  function getRecipesId(){
+    Axios.post(
+      buildPath(`recipe/findAllRecipe?search&page&count`),
+        null,
+        {
+          headers: {
+            authorization: cookies.token,
+          },
+        }
+    )
+    .then((response) => {
+      console.log(response);
+      var res = [];
+
+      for(let i = 0; i < response.data.results.length; i++){
+        res.push(response.data.results[i]._id)
+      }
+
+      setId(res);
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log(error.response.data.error);
+    });
+  }
+
+  function getLikedRecipes(){
+    Axios.post(
+      buildPath(`recipe/getLikedRecipes`),
+        null,
+        {
+          headers: {
+            authorization: cookies.token,
+          },
+        }
+    )
+    .then((response) => {
+      console.log(response);
+      var res = [];
+
+      for(let i = 0; i < response.data.results.length; i++){
+        res.push(response.data.results[i]._id)
+      }
+
+      setLikedId(res);
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log(error.response.data.error);
+    });
+  }
+
+  function getLikedStatus(){
+    for(let i = 0; i < likedId.length; i++){
+      for(let j = 0; j < id.length; j++){
+        if(likedId[i] == id[j])
+          setLiked(true);
+      }
+    }
+
+    console.log(liked);
+  }
+
+  function followPerson() {
+    Axios.post(
+      buildPath("user/followUser"),
+      {
+        userId: idToFollow,
+      },
+      {
+        headers: {
+          authorization: cookies.token,
+        },
+      }
+    )
+      .then((response) => {
+        console.log(response);
+        followChanger();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function unfollowPerson() {
+    Axios.post(
+      buildPath("user/unfollowUser"),
+      {
+        userId: idToFollow,
+      },
+      {
+        headers: {
+          authorization: cookies.token,
+        },
+      }
+    )
+      .then((response) => {
+        console.log(response);
+        followChanger();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function getFollowerCount(){
+    Axios.post(
+      buildPath(`user/getFollowers`),
+        null,
+        {
+          headers: {
+            authorization: cookies.token,
+          },
+        }
+    )
+    .then((response) => {
+      console.log(response);
+      
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log(error.response.data.error);
+    });
+  }
+
+  function getFollowingList(){
+    Axios.post(
+      buildPath("user/getFollowing"),
+      null,
+      {
+        headers: {
+          authorization: cookies.token,
+        },
+      }
+    )
+    .then((response) => {
+      console.log(response);
+      
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log(error.response.data.error);
+    });
+  }
+
   return (
     <Grid item xs={4}>
       <ThemeProvider theme={theme}>
@@ -226,6 +385,10 @@ export default function RecipeCard({
                 event.stopPropagation();
                 event.preventDefault();
                 setOpen(true);
+                getRecipesId();
+                getLikedRecipes();
+                getLikedStatus();
+                getFollowingList();
                 console.log(recipe);
               }}
             >
@@ -245,6 +408,7 @@ export default function RecipeCard({
                     setOpenProfile(true);
                     setClickedUser(clickedUser + 1);
                     getUserRecipes(event.target.innerText);
+                    getFollowerCount();
                   }}
                 >
                   <Avatar
@@ -336,6 +500,7 @@ export default function RecipeCard({
                   setOpenProfile(true);
                   setClickedUser(clickedUser + 1);
                   getUserRecipes(event.target.innerText);
+                  getFollowerCount();
                 }}
               >
                 {recipe.Username}
@@ -416,7 +581,8 @@ export default function RecipeCard({
               }}
               onMouseDown={(event) => event.stopPropagation()}
             />
-            <Button sx={{ color: "white", pl: 2 }}>Follow+</Button>
+            <Button sx={{ color: "white", pl: 2 }} onClick={followPerson}>Follow+</Button>
+            <Button sx={{ color: "white", pl: 2 }} onClick={unfollowPerson}>Unfollow</Button>
           </div>
 
           <DialogContentText className="profileBio">
