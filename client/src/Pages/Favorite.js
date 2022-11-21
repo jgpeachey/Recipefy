952c29a8a2922c5c1c6419f4f96128dbdd09717c
psottomayorpereir <itapeu16@hotmail.com>
2022-11-20 23:22:22 -0500

    Search on favorite page now fully works

--- client/src/Pages/Favorite.js
+++ client/src/Pages/Favorite.js
@@ -34,6 +34,7 @@ export default function Favorite() {
     navigate("/");
   }
   var counter = 0;
+  var refresh = 0;
 
   const [page, setPage] = useState(0);
   console.log(cookies.token);
@@ -44,12 +45,14 @@ export default function Favorite() {
   const [searcher, setSearcher] = useState("");
   const [change, setChange] = useState(false);
   const [recipeCardsArray, setRecipeCardsArray] = useState([]);
+  const [tempRes, setTempRes] = useState([]);
+  let prevSearch;
 
   const [userCards, setUserCards] = useState([]);
   const [clickedUser, setClickedUser] = useState(0);
   const [pfp, setPfp] = useState("");
   const [username, setUsername] = useState("");
-  
+
   var onFavoritePageboolean = true;
 
   const appbarToHome = (appbardata) => {
@@ -136,20 +139,13 @@ export default function Favorite() {
     console.log("pee");
   }
 
-  function getLikedRecipes() {
-    setPage(page + 1);
-    // const config = {
-    //   headers: {
-    //     authorization: cookies.token,
-    //   },
-    //   params: {
-    //     page: page,
-    //     count: 9,
-    //     search: "",
-    //   },
-    // };
-    if (searcher === "") {
-      setPageTitle(cookies.first + "'s Favorite Recipes");
+  function getFilteredRecipes() {
+    setPageTitle("Search Result");
+    setPage(1);
+    console.log("We are searching for " + searcher);
+    console.log(recipeCardsArray);
+    if (recipeCardsArray) {
+      console.log("reupdating recipe cards");
       Axios.post(buildPath(`recipe/getLikedRecipes`), null, {
         headers: {
           authorization: cookies.token,
@@ -164,53 +160,163 @@ export default function Favorite() {
             counter++;
             res.push(response.data.results[q]);
           }
-          if (page === 1) {
-            console.log("cheese");
-            setRecipeCardsArray(res);
-            setChange(!change);
-          }
-          if (res.length != 0) {
-            setRecipeCardsArray(res);
-          }
+
+          console.log("cheese");
+          setTempRes(res);
+          console.log("temp res----> " + tempRes);
+          setChange(!change);
           console.log(recipeCardsArray);
         })
         .catch((error) => {
           console.log(error);
           console.log(error.response.data.error);
         });
+      var finalRes = [];
+      for (let q = 0; q < tempRes.length; q++) {
+        if (tempRes[q].Title.toLowerCase().match(searcher.toLowerCase())) {
+          finalRes.push(tempRes[q]);
+        }
+      }
+      setRecipeCardsArray(finalRes);
+      console.log(searcher);
+      console.log(recipeCardsArray);
+    }
+  }
+
+  function getLikedRecipes() {
+    setPage(page + 1);
+    // const config = {
+    //   headers: {
+    //     authorization: cookies.token,
+    //   },
+    //   params: {
+    //     page: page,
+    //     count: 9,
+    //     search: "",
+    //   },
+    // };
+    Axios.post(buildPath(`recipe/getLikedRecipes`), null, {
+      headers: {
+        authorization: cookies.token,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        var res = [];
+        counter = recipeCardsArray.length;
+        for (let q = 0; q < response.data.results.length; q++) {
+          response.data.results[q].index = counter;
+          counter++;
+          res.push(response.data.results[q]);
+        }
+        if (page === 1) {
+          console.log("cheese");
+          setRecipeCardsArray(res);
+          setChange(!change);
+        }
+        if (res.length != 0) {
+          setRecipeCardsArray(res);
+        }
+        console.log(recipeCardsArray);
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log(error.response.data.error);
+      });
+    if (searcher === "") {
+      setPageTitle(cookies.first + "'s Favorite Recipes");
+      // Axios.post(buildPath(`recipe/getLikedRecipes`), null, {
+      //   headers: {
+      //     authorization: cookies.token,
+      //   },
+      // })
+      //   .then((response) => {
+      //     console.log(response);
+      //     var res = [];
+      //     counter = recipeCardsArray.length;
+      //     for (let q = 0; q < response.data.results.length; q++) {
+      //       response.data.results[q].index = counter;
+      //       counter++;
+      //       res.push(response.data.results[q]);
+      //     }
+      //     if (page === 1) {
+      //       console.log("cheese");
+      //       setRecipeCardsArray(res);
+      //       setChange(!change);
+      //     }
+      //     if (res.length != 0) {
+      //       setRecipeCardsArray(res);
+      //     }
+      //     console.log(recipeCardsArray);
+      //   })
+      //   .catch((error) => {
+      //     console.log(error);
+      //     console.log(error.response.data.error);
+      //   });
     } else {
       setPageTitle("Search Result");
       setPage(1);
-      console.log("here");
-      Axios.post(
-        buildPath(
-          `recipe/findRecipe?page=${page}&&count=${9}&search=${searcher}`
-        ),
-        null,
-        {
-          headers: {
-            authorization: cookies.token,
-          },
-        }
-      )
-        .then((response) => {
-          console.log(response);
-          var res = [];
-          counter = recipeCardsArray.length;
-          for (let q = 0; q < response.data.results.length; q++) {
-            response.data.results[q].index = counter;
-            counter++;
-            res.push(response.data.results[q]);
-          }
-          if (res.length != 0) {
-            setRecipeCardsArray(res);
-          }
-          console.log(recipeCardsArray);
-        })
-        .catch((error) => {
-          console.log(error);
-          console.log(error.response.data.error);
-        });
+      // console.log("here");
+      // Axios.post(
+      //   buildPath(
+      //     `recipe/findRecipe?page=${page}&&count=${9}&search=${searcher}`
+      //   ),
+      //   null,
+      //   {
+      //     headers: {
+      //       authorization: cookies.token,
+      //     },
+      //   }
+      // )
+      //   .then((response) => {
+      //     console.log(response);
+      //     var res = [];
+      //     counter = recipeCardsArray.length;
+      //     for (let q = 0; q < response.data.results.length; q++) {
+      //       response.data.results[q].index = counter;
+      //       counter++;
+      //       res.push(response.data.results[q]);
+      //     }
+      //     if (res.length != 0) {
+      //       setRecipeCardsArray(res);
+      //     }
+      //     console.log(recipeCardsArray);
+      //   })
+      //   .catch((error) => {
+      //     console.log(error);
+      //     console.log(error.response.data.error);
+      //   });
+      /////////////////////////////////////////////////////////
+      // if (recipeCardsArray) {
+      //   var res = [];
+      //   for (let q = 0; q < recipeCardsArray.length; q++) {
+      //     console.log(recipeCardsArray[q].Title);
+      //     if (
+      //       recipeCardsArray[q].Title.toLowerCase().match(
+      //         searcher.toLowerCase()
+      //       )
+      //     ) {
+      //       res.push(recipeCardsArray[q]);
+      //     }
+      //   }
+      //   console.log(searcher);
+      //   setRecipeCardsArray(res);
+      //   console.log(res);
+      // }
+      /////////////////////////////////////////////////////////
+      // if (recipeCardsArray) {
+      //   var res = [];
+
+      //   setRecipeCardsArray(
+      //     recipeCardsArray.filter((recipe) =>
+      //       recipe.Title.toLowerCase().match(searcher.toLowerCase())
+      //     )
+      //   );
+      //   console.log(searcher);
+      //   console.log(recipeCardsArray);
+      // }
+
+      /////////////////////////////////////////////////////////
       // Axios.get(buildPath("recipe/findAllRecipe"), config)
       //   .then((response) => {
       //     console.log(response);
@@ -231,9 +337,27 @@ export default function Favorite() {
   }
 
   useDeepCompareEffect(() => {
+    if (refresh === 0) {
+      prevSearch = "";
+      refresh++;
+    }
     console.log("called");
-    getLikedRecipes();
-  }, [searcher, recipeCardsArray]);
+    console.log(prevSearch);
+    console.log(searcher);
+    if (!(searcher === "")) {
+      if (prevSearch != searcher) {
+        console.log("CHANGEE");
+        prevSearch = searcher;
+        getFilteredRecipes() && getLikedRecipes();
+      }
+    } else {
+      prevSearch = "";
+    }
+    if (prevSearch === "") {
+      console.log("EMPTY");
+      getLikedRecipes();
+    }
+  }, [searcher, recipeCardsArray, tempRes]);
 
   return (
     <ThemeProvider theme={theme}>
